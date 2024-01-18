5f26e04904aa4e739efee0155ed4d685820440aa
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2024-01-17 23:55:39 -0300

    Add verify tests

---
+++ test/verify.test.ts
@@ -0,0 +1,133 @@
+// typescript-opentimestamps: An OpenTimestamps client written in TypeScript.
+// Copyright (C) 2024  La Crypta
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'use strict';
+
+import type { Leaf } from '../src/types';
+
+import { newEdges, newLeaves } from '../src/internals';
+import { uint8ArrayFromHex } from '../src/utils';
+import { verifyTimestamp } from '../src/verify';
+
+describe('Verify', () => {
+  describe('verifyTimestamp()', () => {
+    it.each([
+      {
+        bitcoinVerifierResponse: null,
+        litecoinVerifierResponse: null,
+        bitcoinVerifierError: 'something',
+        litecoinVerifierError: 'else',
+        expected: {
+          attestations: {},
+          errors: {
+            bitcoinVerifier: [new Error('Error (bitcoinVerifier): unknown error in verifier')],
+            litecoinVerifier: [new Error('Error (litecoinVerifier): unknown error in verifier')],
+          },
+        },
+        name: 'should accumulate unknown errors per verifier',
+      },
+      {
+        bitcoinVerifierResponse: null,
+        litecoinVerifierResponse: null,
+        bitcoinVerifierError: new Error('something'),
+        litecoinVerifierError: new Error('else'),
+        expected: {
+          attestations: {},
+          errors: {
+            bitcoinVerifier: [new Error('Error (bitcoinVerifier): something')],
+            litecoinVerifier: [new Error('Error (litecoinVerifier): else')],
+          },
+        },
+        name: 'should accumulate errors per verifier',
+      },
+      {
+        bitcoinVerifierResponse: 123456,
+        litecoinVerifierResponse: 789012,
+        bitcoinVerifierError: null,
+        litecoinVerifierError: null,
+        expected: {
+          attestations: {
+            123456: ['bitcoinVerifier'],
+            789012: ['litecoinVerifier'],
+          },
+          errors: {},
+        },
+        name: 'should accumulate attestations per time',
+      },
+    ])(
+      '$name',
+      ({
+        bitcoinVerifierResponse,
+        litecoinVerifierResponse,
+        bitcoinVerifierError,
+        litecoinVerifierError,
+        expected,
+      }:
+        | {
+            bitcoinVerifierResponse: number | undefined;
+            litecoinVerifierResponse: number | undefined;
+            bitcoinVerifierError: null;
+            litecoinVerifierError: null;
+            expected: { attestations: Record<number, string[]>; errors: Record<string, Error[]> };
+          }
+        | {
+            bitcoinVerifierResponse: null;
+            litecoinVerifierResponse: null;
+            bitcoinVerifierError: Error | string;
+            litecoinVerifierError: Error | string;
+            expected: { attestations: Record<number, string[]>; errors: Record<string, Error[]> };
+          }) => {
+        void expect(
+          verifyTimestamp(
+            {
+              version: 1,
+              fileHash: {
+                algorithm: 'sha1',
+                value: uint8ArrayFromHex('00112233445566778899aabbccddeeff00112233'),
+              },
+              tree: {
+                edges: newEdges(),
+                leaves: newLeaves().add({ type: 'bitcoin', height: 123 }).add({ type: 'litecoin', height: 123 }),
+              },
+            },
+            {
+              bitcoinVerifier: (_msg: Uint8Array, leaf: Leaf): Promise<number | undefined> => {
+                if ('bitcoin' !== leaf.type) {
+                  return Promise.resolve(undefined);
+                }
+                if (null !== bitcoinVerifierError) {
+                  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+                  throw bitcoinVerifierError;
+                }
+                return Promise.resolve(bitcoinVerifierResponse);
+              },
+              litecoinVerifier: (_msg: Uint8Array, leaf: Leaf): Promise<number | undefined> => {
+                if ('litecoin' !== leaf.type) {
+                  return Promise.resolve(undefined);
+                }
+                if (null !== litecoinVerifierError) {
+                  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+                  throw litecoinVerifierError;
+                }
+                return Promise.resolve(litecoinVerifierResponse);
+              },
+            },
+          ),
+        ).resolves.toStrictEqual(expected);
+      },
+    );
+  });
+});
