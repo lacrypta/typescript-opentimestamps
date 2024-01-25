81df2e0fa01e6e88b4a1fbfd749acd5cfc860e1b
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2024-01-25 16:12:14 -0300

    Add read module module-level documentation

--- src/read.ts
+++ src/read.ts
@@ -14,6 +14,13 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+/**
+ * This module exposes binary reading functions.
+ *
+ * @packageDocumentation
+ * @module
+ */
+
 'use strict';
 
 import type { Edge, FileHash, Leaf, Timestamp, Tree } from './types';
